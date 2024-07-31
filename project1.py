88ffbf0a0eb5e6bba43a382d4827d418906870e1
Sanjay p <162776443+Sanjay-Paul-Murugan@users.noreply.github.com>
2024-07-31 15:49:55 +0000

    added css to the site

--- project1.py
+++ project1.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app  =Flask(__name__)
 @app.route('/')
 def hello_world():
-  return "hello mapla"
+  return render_template("homepage.html")
 
 if __name__ == "__main__":
   app.run("0.0.0.0",True)
